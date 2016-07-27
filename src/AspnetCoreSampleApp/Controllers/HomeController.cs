59e2db3869a9be62349c44fd020e992c69842ab7
Jeroen Hildering <j.hildering@live.nl>
2016-07-27 14:15:00 +0200

    Created sample app

---
+++ src/AspnetCoreSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreSampleApp.Controllers
+{
+    [Route("api/[controller]"), Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World! [Asp.NET Core]";
+        }
+    }
+}
